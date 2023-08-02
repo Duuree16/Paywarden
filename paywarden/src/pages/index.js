8679d6828408e6bb6506bdec719104eeaf3f6ea3
duuree17 <duuren0315@gmail.com>
2023-08-02 22:29:40 +0800

    new base

---
+++ paywarden/src/pages/index.js
@@ -0,0 +1,10 @@
+import styles from "../designs/mainPage.module.css"
+
+export default function Home() {
+  
+  return (
+    <div className={styles.base}>
+      Erdemee GG EZ fk u
+    </div>
+  );
+}
