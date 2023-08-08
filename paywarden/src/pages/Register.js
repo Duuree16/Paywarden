4304f112aa79b35fe46f67b3603af4d0b5093308
duuree17 <duuren0315@gmail.com>
2023-08-09 01:40:29 +0800

    noice2

--- paywarden/src/pages/Register.js
+++ paywarden/src/pages/Register.js
@@ -1,16 +1,24 @@
 import Link from 'next/link'
 import styles from '../designs/login.module.css'
+import { useState } from 'react'
 
 export default function Register() {
+  const [name, setName] = useState('')
+  const [pass, setPass] = useState('')
+  const [vis,setVis] = useState(false)
+  const Login = () => {
+    console.log(name,pass)
+  }
   return (
     <div className={styles.base}>
       <div className={styles.body}>
 
       <Link href="/" style={{textDecoration:"none"}}><h1 className={styles.Title}>Very Good Web App</h1></Link>{' '}
       <h3 className={styles.title}>Register</h3>
-      <input placeholder='Username' className={styles.username}/>
-      <input placeholder='Password'className={styles.username} />
-      <button className={styles.button}>Log In</button>
+      <input placeholder='Username' className={styles.username} onChange={(e)=> setName(e.target.value)}/>
+      <input type={!vis?'password': "text"} placeholder='Password'className={styles.username} onChange={(e)=> setPass(e.target.value)} />
+      <button className={styles.button} onClick={()=> Login()}>Log In</button>
+      <h2 className={styles.eyeshit} onClick={()=> setVis(!vis)}>👁</h2>
       </div>
     </div>
   )
