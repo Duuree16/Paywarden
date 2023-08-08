658cd3c7eef76fa65747504b49dfdafff8d57a73
duuree17 <duuren0315@gmail.com>
2023-08-09 02:21:10 +0800

    PROGRESS

--- paywarden/src/common/userContext.js
+++ paywarden/src/common/userContext.js
@@ -1,16 +1,26 @@
-import { createContext, useEffect } from "react";
-export const UserContext = createContext({});
+import axios from 'axios'
+import { createContext, useEffect } from 'react'
+export const UserContext = createContext({})
 export const UserProvider = ({ children }) => {
-  const userVerification = () =>{
-    const token = localStorage.getItem("auth token")
+  const userVerification = () => {
+    const token = localStorage.getItem('auth token')
     console.log(token)
+    if(token){
+      axios
+      .get('http://localhost:1234/verify', {
+        headers: { Authorization: token },
+      })
+      .then((res) => {
+        console.log(res.data)
+      })
+    }
   }
-  useEffect(()=>{
+  useEffect(() => {
     userVerification()
-  },[])
+  }, [])
   return (
-    <UserContext.Provider value={{ userVerification}}>
+    <UserContext.Provider value={{ userVerification }}>
       {children}
     </UserContext.Provider>
-  );
-};
+  )
+}
