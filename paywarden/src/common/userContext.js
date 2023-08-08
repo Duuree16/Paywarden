2b164e43f5820e89e609368b8d8b5607c78a91fd
duuree17 <duuren0315@gmail.com>
2023-08-09 02:16:02 +0800

    nice

--- paywarden/src/common/userContext.js
+++ paywarden/src/common/userContext.js
@@ -1,9 +1,15 @@
-import { createContext } from "react";
+import { createContext, useEffect } from "react";
 export const UserContext = createContext({});
 export const UserProvider = ({ children }) => {
-  
+  const userVerification = () =>{
+    const token = localStorage.getItem("auth token")
+    console.log(token)
+  }
+  useEffect(()=>{
+    userVerification()
+  },[])
   return (
-    <UserContext.Provider value={{ }}>
+    <UserContext.Provider value={{ userVerification}}>
       {children}
     </UserContext.Provider>
   );
