1a81e228709309a20d787f3023a29b8c10947c27
duuree17 <duuren0315@gmail.com>
2023-08-10 23:13:52 +0800

    Yes

--- paywarden/src/common/userContext.js
+++ paywarden/src/common/userContext.js
@@ -3,6 +3,7 @@ import { createContext, useEffect } from 'react'
 export const UserContext = createContext({})
 //My name is danny
 //im doing useless stuff just for my git repo
+//jk
 export const UserProvider = ({ children }) => {
   const userVerification = () => {
     const token = localStorage.getItem('auth token')
